df30c3a1cb0db89141ce936b80349b47594a8eef
Madhavaraochalla <challa.madhavarao2016@gmail.com>
2025-04-25 01:51:45 +0530

    Commit 28: create-users.sh push-to-github.sh

--- push-to-github.sh
+++ push-to-github.sh
@@ -17,7 +17,7 @@ AUTH_REMOTE_URL="https://${GITHUB_USER}:${GITHUB_TOKEN}@github.com/${GITHUB_USER
 FILES=("create-users.sh" "deploy-grafana.sh" "push-to-github.sh" "Readme.md")
 
 # Files to remove (if needed)
-FILES_TO_REMOVE=("create-users.sh")
+FILES_TO_REMOVE=("")
 
 # Git config to fix line ending warnings
 git config --global core.autocrlf input
